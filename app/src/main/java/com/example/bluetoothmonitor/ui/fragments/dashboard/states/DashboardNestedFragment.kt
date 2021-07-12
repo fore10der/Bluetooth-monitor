e2be9eeabac56fafef37e3d85dd81b78dcd78c03
oberdev <qovalski@gmail.com>
2021-07-13 02:35:22 +0300

    simplify bit nested fragment code

--- app/src/main/java/com/example/bluetoothmonitor/ui/fragments/dashboard/states/DashboardNestedFragment.kt
+++ app/src/main/java/com/example/bluetoothmonitor/ui/fragments/dashboard/states/DashboardNestedFragment.kt
@@ -1,9 +1,10 @@
 package com.example.bluetoothmonitor.ui.fragments.dashboard.states
 
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.example.bluetoothmonitor.ui.fragments.dashboard.DashboardFragment
 
-sealed class DashboardNestedFragment: Fragment() {
+sealed class DashboardNestedFragment @JvmOverloads constructor(@LayoutRes contentLayoutId: Int = 0): Fragment(contentLayoutId) {
     val parent: DashboardFragment
         get() = requireParentFragment().requireParentFragment() as DashboardFragment
 }
